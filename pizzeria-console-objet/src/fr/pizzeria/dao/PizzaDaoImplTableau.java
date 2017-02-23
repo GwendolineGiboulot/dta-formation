29162079b564bcc076cb1ecdb73056b7eb15bd00
Valentin Giboulot <valentin.giboulot@gmail.com>
2017-02-23 09:37:48 +0100

    tp 5 premier jet

--- pizzeria-console-objet/src/fr/pizzeria/dao/PizzaDaoImplTableau.java
+++ pizzeria-console-objet/src/fr/pizzeria/dao/PizzaDaoImplTableau.java
@@ -8,6 +8,19 @@ public class PizzaDaoImplTableau implements IPizzaDao {
 
 	private static int tailleTableau = 0;
 
+	static {
+		tableauPizza[0] = new Pizza(0, "PEP", "Pépéroni", 12.50);
+		tableauPizza[1] = new Pizza(1, "MAR", "Margherita", 14.00);
+		tableauPizza[2] = new Pizza(2, "REI", "La Reine", 11.50);
+		tableauPizza[3] = new Pizza(3, "FRO", "La 4 fromages", 12.00);
+		tableauPizza[4] = new Pizza(4, "CAN", "La cannibale", 12.50);
+		tableauPizza[5] = new Pizza(5, "SAV", "La savoyarde", 13.00);
+		tableauPizza[6] = new Pizza(6, "ORI", "L’orientale", 13.50);
+		tableauPizza[7] = new Pizza(7, "IND", "L’indienne", 14.00);
+
+		tailleTableau = 8;
+	}
+
 	@Override
 	public Pizza[] findAllPizzas() {
 		Pizza tableauARenvoyer[] = new Pizza[tailleTableau];
