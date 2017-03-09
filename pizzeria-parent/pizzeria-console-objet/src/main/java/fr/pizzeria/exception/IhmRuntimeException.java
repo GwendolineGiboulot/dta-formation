953fbe73734407913fb9164f7bdd4a38df8803e5
Valentin Giboulot <valentin.giboulot@gmail.com>
2017-03-09 09:34:40 +0100

    rajout d'un peu de doc

--- pizzeria-parent/pizzeria-console-objet/src/main/java/fr/pizzeria/exception/IhmRuntimeException.java
+++ pizzeria-parent/pizzeria-console-objet/src/main/java/fr/pizzeria/exception/IhmRuntimeException.java
@@ -1,5 +1,9 @@
 package fr.pizzeria.exception;
 
+/**
+ * @author Quelqun
+ *
+ */
 public class IhmRuntimeException extends RuntimeException {
 
 	private static final long serialVersionUID = -5333919020370360569L;
@@ -9,15 +13,25 @@ public class IhmRuntimeException extends RuntimeException {
 
 	}
 
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
 	public IhmRuntimeException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
 
 	}
 
+	/**
+	 * @param message
+	 */
 	public IhmRuntimeException(String message) {
 		super(message);
 	}
 
+	/**
+	 * @param cause
+	 */
 	public IhmRuntimeException(Throwable cause) {
 		super(cause);
 
--- pizzeria-parent/pizzeria-dao/src/main/java/fr/pizzeria/dao/IDao.java
+++ pizzeria-parent/pizzeria-dao/src/main/java/fr/pizzeria/dao/IDao.java
@@ -6,14 +6,36 @@ import fr.pizzeria.exception.DeleteDaoException;
 import fr.pizzeria.exception.SaveDaoException;
 import fr.pizzeria.exception.UpdateDaoException;
 
+/**
+ * @author Quelqun
+ *
+ * @param <E>
+ * @param <Id>
+ */
 public interface IDao<E, Id> {
 
+	/**
+	 * @return
+	 */
 	List<E> findAllPizzas();
 
+	/**
+	 * @param pizza
+	 * @throws SaveDaoException
+	 */
 	void saveNewPizza(E pizza) throws SaveDaoException;
 
+	/**
+	 * @param code
+	 * @param pizza
+	 * @throws UpdateDaoException
+	 */
 	void updatePizza(Id code, E pizza) throws UpdateDaoException;
 
+	/**
+	 * @param code
+	 * @throws DeleteDaoException
+	 */
 	void deletePizza(Id code) throws DeleteDaoException;
 
 }
